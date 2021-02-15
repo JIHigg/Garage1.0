309fa4a5f92d68e383672655c0e15c45513c801b
higginsdude@gmail.com <higginsdude@gmail.com>
2021-02-15 17:07:53 +0100

    Added Unit Tests

---
+++ UnitTests/GarageHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Garage1._0;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage1._0.Tests
+{
+    [TestClass()]
+    public class GarageHandlerTests
+    {
+        [TestMethod()]
+        public void Display_OneTypeVehicle_Test()
+        {
+            //Assemble
+            GarageHandler gh = new GarageHandler();
+            
+            Car c1 = new Car(23, 4);
+            Car c2 = new Car(32, 8);
+            Submarine s1 = new Submarine(12.3);
+            Submarine s2 = new Submarine(8);
+            gh.AddVehicle(c2);
+            gh.AddVehicle(s2);
+            gh.AddVehicle(s1);
+            gh.AddVehicle(c1);
+
+            int noOfCars = 2;
+
+            //Action
+            gh.DisplayOneTypeVehicle('5');
+
+            //Assert
+            Assert.AreEqual(noOfCars, gh.total);
+        }
+    }
+}
---
+++ UnitTests/GarageTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Garage1._0;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage1._0.Tests
+{
+    [TestClass()]
+    public class GarageTests
+    {
+        [TestMethod()]
+        public void NewGarage_NotSame_Test()
+        {
+            Garage<Vehicle> G = new Garage<Vehicle>();
+
+            Garage<Car> vehicles = new Garage<Car>(4);
+            Garage<Vehicle> v2 = new Garage<Vehicle>(5);
+
+            Assert.AreNotSame(vehicles, v2);
+        }
+
+        [TestMethod()]
+        public void GarageTest()
+        {
+            //
+        }
+
+        [TestMethod()]
+        public void AddVehicleTest_Car()
+        {
+            bool results;
+            Garage<Vehicle> G = new Garage<Vehicle>();
+            Car c1 = new Car(100, 4);
+            Car c2 = new Car(24.9, 8);
+
+            G.AddVehicle(c1);
+            G.AddVehicle(c2);
+
+            results = G.G[0].Equals(c1);
+
+
+            Assert.IsTrue(results);
+        }
+
+        [TestMethod()]
+        public void GetVehicleTest()
+        {
+            Garage<Vehicle> TestGarage = new Garage<Vehicle>();
+            Car car = new Car(30, 5);
+            Boat boat = new Boat("Rowboat");
+            Motorcycle motorcycle = new Motorcycle(150);
+
+            TestGarage.AddVehicle(car);
+            TestGarage.AddVehicle(boat);
+            TestGarage.AddVehicle(motorcycle);
+
+            var results = TestGarage.GetVehicle(boat);
+            Assert.AreEqual(boat, results);
+        }
+
+        
+    }
+}
